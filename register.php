c875e8f017654fdc4612719daee2e49836e527c5
lukeknows <luke.knows.1@gmail.com>
2024-12-02 20:25:48 -0500

    added update_information.php and parent's info to register.php

--- register.php
+++ register.php
@@ -6,6 +6,7 @@
 	$address = $inData['address'] . " " . $inData['city'] . " " . $inData['state'] . " " . $inData['zip'];
 	$phone = $inData['phone'];
 	$available_hours = $inData['available_hours'];
+	$parent_info = $inData['parent_info']; // I guess we'll just make this a string, should probably be a text area
 	$care_dollars = 2000;
 
 	// connect to DB
@@ -17,7 +18,7 @@
 	else
 	{
 		// Check if user is already in Database
-        $CMD = $SQL_DB->prepare("SELECT username FROM Users WHERE username=?");
+        $CMD = $conn->prepare("SELECT username FROM Users WHERE username=?");
         $CMD->bind_param("s", $username);
 		$CMD->execute();
 		$result = $CMD->get_result();
@@ -29,8 +30,8 @@
 			$_SESSION['user_id'] = $user_id; // Record user's session
 
 			// insert user into database
-			$stmt = $conn->prepare("INSERT into Users (id,username,password,address,phone,availableHours,careDollars) VALUES(?,?,?,?,?,?,?)");
-			$stmt->bind_param("sssssii", $user_id, $username, $password, $address, $phone, $available_hours, $care_dollars);
+			$stmt = $conn->prepare("INSERT into Users (id,username,password,address,phone,availableHours,careDollars,parentInfo) VALUES(?,?,?,?,?,?,?,?)");
+			$stmt->bind_param("sssssiis", $user_id, $username, $password, $address, $phone, $available_hours, $care_dollars, $parent_info);
 			$stmt->execute();
 			$stmt->close();
 			$conn->close();
